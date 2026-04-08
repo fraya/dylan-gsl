dfaf69ce1ecc437da6029d619d908b8f51fd5bd8
Fernando Raya <fernandoraya@gmail.com>
2026-04-08 22:22:44 +0200

    fix(complex): Rename macros to clarify purpose

--- src/lib/complex-shim.c
+++ src/lib/complex-shim.c
@@ -7,18 +7,18 @@ void gsl_complex_polar_shim (double r, double i, double* rr, double* ii) {
   *ii = GSL_IMAG(c);
 }
 
-#define DEFINE_GSL_SHIM_UNARY(name) \
+#define DEFINE_GSL_COMPLEX_PROPERTY_SHIM(name) \
 double gsl_complex_##name##_shim (double r, double i) { \
   gsl_complex z = gsl_complex_rect(r, i); \
   return gsl_complex_##name(z); \
 }
 
-DEFINE_GSL_SHIM_UNARY(arg)
-DEFINE_GSL_SHIM_UNARY(abs)
-DEFINE_GSL_SHIM_UNARY(abs2)
-DEFINE_GSL_SHIM_UNARY(logabs)
+DEFINE_GSL_COMPLEX_PROPERTY_SHIM(arg)
+DEFINE_GSL_COMPLEX_PROPERTY_SHIM(abs)
+DEFINE_GSL_COMPLEX_PROPERTY_SHIM(abs2)
+DEFINE_GSL_COMPLEX_PROPERTY_SHIM(logabs)
 
-#define DEFINE_GSL_SHIM_BIN(name) \
+#define DEFINE_GSL_COMPLEX_BINARY_OPERATOR_SHIM(name) \
 void gsl_complex_##name##_shim (double r1, double i1, double r2, double i2, double *r, double *i) { \
   gsl_complex a = gsl_complex_rect(r1, i1); \
   gsl_complex b = gsl_complex_rect(r2, i2); \
@@ -27,15 +27,15 @@ void gsl_complex_##name##_shim (double r1, double i1, double r2, double i2, doub
   *i = GSL_IMAG(c); \
 }
 
-DEFINE_GSL_SHIM_BIN(add)
-DEFINE_GSL_SHIM_BIN(sub)
-DEFINE_GSL_SHIM_BIN(mul)
-DEFINE_GSL_SHIM_BIN(div)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_SHIM(add)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_SHIM(sub)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_SHIM(mul)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_SHIM(div)
 
 // Macro to make operations (+,-,*,/) between a complex number and a
 // double (real o imaginary part)
 
-#define DEFINE_GSL_SHIM_BIN_REAL(name) \
+#define DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(name) \
 void gsl_complex_##name##_shim (double r1, double i1, double x, double *r, double *i) { \
   gsl_complex a = gsl_complex_rect(r1, i1); \
   gsl_complex b = gsl_complex_##name(a, x); \
@@ -43,13 +43,13 @@ void gsl_complex_##name##_shim (double r1, double i1, double x, double *r, doubl
   *i = GSL_IMAG(b); \
 }
 
-DEFINE_GSL_SHIM_BIN_REAL(add_real)
-DEFINE_GSL_SHIM_BIN_REAL(sub_real)
-DEFINE_GSL_SHIM_BIN_REAL(mul_real)
-DEFINE_GSL_SHIM_BIN_REAL(div_real)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(add_real)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(sub_real)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(mul_real)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(div_real)
 
-DEFINE_GSL_SHIM_BIN_REAL(add_imag)
-DEFINE_GSL_SHIM_BIN_REAL(sub_imag)
-DEFINE_GSL_SHIM_BIN_REAL(mul_imag)
-DEFINE_GSL_SHIM_BIN_REAL(div_imag)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(add_imag)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(sub_imag)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(mul_imag)
+DEFINE_GSL_COMPLEX_BINARY_OPERATOR_WITH_REAL_SHIM(div_imag)
 
